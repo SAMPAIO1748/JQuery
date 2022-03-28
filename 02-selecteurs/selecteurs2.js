4a2fd7a09c5344d6bae51c24e5ba75187cc806d1
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-03-28 13:45:36 +0200

    selecteurs2 fin

--- 02-selecteurs/selecteurs2.js
+++ 02-selecteurs/selecteurs2.js
@@ -5,9 +5,61 @@
 
 $(document).ready(function () {
   // quand le document est entièrement chargé
+
   // --------------------------
   // Selection par l'id
   // --------------------------
-  // exercice selectionner l'élement qui a l'id "maSection"
+
+  // exercice selectionner l'élement qui a l'id "maSection",
   // remplacer le texte d'origine par "<h2>Bonjour à tous</h2>"
+
+  var maSection = $("#maSection");
+
+  console.log(maSection.text());
+
+  maSection.html("<h2>Bonjour à tous</h2>");
+
+  // --------------------------------------
+  // Selection par la classe
+  // --------------------------------------
+
+  // exercice : selectionner les div qui ont la classe mesDivs
+  // appliquer du css :
+  /*
+    padding: 20px,
+    backgroundcolor :'red',
+    margin-top: 10px,
+    color : navy
+  */
+
+  var mesDivs = $(".mesDivs");
+  mesDivs.css("padding", "20px");
+  mesDivs.css({ backgroundColor: "red", "margin-top": "10px", color: "navy" });
+
+  // -----------------------------------
+  // Selection par le nom de balise
+  // -----------------------------------
+
+  //exercice sur les p appliquer le css :
+  /*
+    font-family : 'calibri',
+    font-size : "21px",
+    color : "orange",
+    background-color : 'lightblue',
+    padding : '10px'
+  */
+
+  $("p").css({
+    "font-family": "calibri",
+    fontSize: "21px",
+    color: "orange",
+    "background-color": "lightblue",
+    padding: "10px",
+  });
+
+  // ------------------------------------
+  // SELECTEUR MULTIPLE
+  // ------------------------------------
+
+  console.log($("span, div, p"));
 });
