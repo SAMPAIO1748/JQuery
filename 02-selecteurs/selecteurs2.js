1d6ac163c35ec98447b4f4314c018ec3836abf03
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-03-28 11:50:05 +0200

    selecteurs2

---
+++ 02-selecteurs/selecteurs2.js
@@ -0,0 +1,13 @@
+// Pour commencer à écrire du JS avec JQuery, il est recommandé de toujours débuter avec un document ready
+// $ représente l'outil JQuery, lorsque l'on utilise JQuery, on commence toujours par $.
+// Attention, en cas de conflit dû à l'appel de plusieurs bibliothèques JavaScript dans une page web, cela créer un conflit et
+// et le $() ne focntionne plus. Dans ce cas il est possible d'utiliser jQuery()
+
+$(document).ready(function () {
+  // quand le document est entièrement chargé
+  // --------------------------
+  // Selection par l'id
+  // --------------------------
+  // exercice selectionner l'élement qui a l'id "maSection"
+  // remplacer le texte d'origine par "<h2>Bonjour à tous</h2>"
+});
